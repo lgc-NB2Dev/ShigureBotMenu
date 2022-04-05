f83b07960b55d94a4d0aeb5214962ec313352015
student_2333 <lgc2333@126.com>
2022-04-05 14:06:14 +0800

    modify

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,42 @@ window.$docsify = {
   notFoundPage: true,
   topMargin: 90,
   repo: 'https://github.com/lgc2333/ShigureBotMenu',
-  //logo: '/imgs/shigure.jpg',
   name: 'ShigureBot 功能列表',
   formatUpdated: '{YYYY}/{MM}/{DD} {HH}:{mm}:{ss}',
+  //logo: '/imgs/shigure.jpg',
   search: {
     placeholder: '搜索……',
     noData: '没有发现结果呢',
+  },
+  progress: {
+    color: 'var(--highlightColor,#42b983)',
+  },
+  copyCode: {
+    buttonText: '📑',
+    errorText: '❌失败！',
+    successText: '✅',
+  },
+  'flexible-alerts': {
+    style: 'flat',
+    note: {
+      label: '说明：',
+    },
+    tip: {
+      label: '提示：',
+    },
+    warning: {
+      label: '警告！',
+    },
+    attention: {
+      label: '禁止！！',
+    },
+  },
+  Valine: {
+    appId: 'SWMnSx0tSjPO8b3RPBHhX6K6-MdYXbMMI',
+    appKey: '7r6slQi7BJMEr4lSlPHwE1fK',
+    placeholder: '快来写个评论吧！',
+    avatar: 'identicon',
+    enableQQ: true,
+    requiredFields: ['nick', 'mail'],
   },
 };
